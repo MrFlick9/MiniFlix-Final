3104a720a22aca0e1404a08c410d8d6b60a538f4
MrFlick9 <85364978+MrFlick9@users.noreply.github.com>
2021-06-10 12:06:56 +0530

    Update urls.py

--- MovieHunter/urls.py
+++ MovieHunter/urls.py
@@ -17,11 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.shortcuts import render
 from . import views
+from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^movie/', include('movie.urls')),
     url(r'^user/', include('user.urls')),
     url(r'^$', views.index, name='index'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
     # url(r'.*', lambda request: render(request, '404.html'), name='404'),
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
